b149e36d3eb810e61f1441b6340573c85950d2f9
Alessandro Versace <aleversace98@gmail.com>
2019-09-19 15:20:08 +0200

    Code modularity added + todo.md

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# Python Port Scanner
+
+Written in Python3.
+
+## Todo list
+
+* [Change Host]() - Add option to change host.
+* [Open ports]() - Return only open ports after a scan.
+* [Is it working?]() - Checks if all works properly... Not sure about it.
--- portScanner.py
+++ portScanner.py
@@ -8,6 +8,7 @@ def portScan(port):
         print('The port is open')
 
 
+# Menu with lists of options
 def menu():
     print('What do you want to do? Here a list of options:\n')
     print('[1] Scan a single port\n')
@@ -18,10 +19,50 @@ def menu():
     return action
 
 
+# Function to scan a single port
+def scanOnePort(host):
+    port = int(input('Enter the port to scan: '))
+    print('Scanning port %s...' % str(port))
+    portScan(port)
+    print('\n')
+
+
+# Function to scan a series of ports
+def scanSomePorts(host):
+    ports_name = input(
+        'Insert a list of ports following this format: ##, ##, ###, #, ####\n')
+    ports = ports_name.split(', ')  # List of strings
+
+    # Converts from a list of string to a list of int
+    ports_int = []
+
+    for port in ports:
+        ports_int.append(int(port))
+
+    # Checks every single port
+    for port in ports_int:
+        print('Scanning port %s...' % str(port))
+        portScan(port)
+
+    print('\n')
+
+
+# TODO: return just opened ports in a list/set
+def scanAllPorts(host):
+    for i in range(1, 49151):
+        port = i
+        print('Scanning port %s...' % str(port))
+        portScan(port)
+
+    print('\n')
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Set timeout to skip longer scan port
 # s.settimeout(10)
+
+host = input('Enter the IP to scan: ')
 
 while True:
     action = menu()
@@ -29,40 +70,12 @@ while True:
     if action == 0:
         break
     elif action == 1:           # Accept one port
-        host = input('Enter the IP to scan: ')
-        port = int(input('Enter the port to scan: '))
-        print('Scanning port %s...' % str(port))
-        portScan(port)
-
-        print('\n')
+        scanOnePort(host)
     elif action == 2:           # Accept indefined number of ports
-        host = input('Enter the IP to scan: ')
-        ports_name = input(
-            'Insert a list of ports following this format: ##, ##, ###, #, ####\n')
-        ports = ports_name.split(', ')  # List of strings
-
-        # Converts from a list of string to a list of int
-        ports_int = []
-
-        for port in ports:
-            ports_int.append(int(port))
-
-        # Checks every single port
-        for port in ports_int:
-            print('Scanning port %s...' % str(port))
-            portScan(port)
-
-        print('\n')
+        scanSomePorts(host)
     elif action == 3:           # Checks every single port from 1 to 49151
-        host = input('Enter the IP to scan: ')
-
-        # TODO: return just opened ports in a list/set
-        for i in range(1, 49151):
-            port = i
-            print('Scanning port %s...' % str(port))
-            portScan(port)
-        
-        print('\n')
+        scanAllPorts(host)
+    # TODO: Add option to change host.
     else:
         print('This is not a correct option.\n')
         print('\n')
